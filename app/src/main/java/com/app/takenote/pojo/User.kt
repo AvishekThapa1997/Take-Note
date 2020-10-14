d239c49bc45e88c311d69b5e3d1ed5c1effac4a7
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-10-14 10:47:35 +0530

    secondary constructor added

--- app/src/main/java/com/app/takenote/pojo/User.kt
+++ app/src/main/java/com/app/takenote/pojo/User.kt
@@ -1,3 +1,37 @@
 package com.app.takenote.pojo
 
-data class User(val fullName: String, val email: String, val password: String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User(
+    val fullName: String?,
+    val email: String?,
+    val password: String?,
+    val imageUri: String?
+) : Parcelable {
+    constructor(fullName: String, email: String) : this(fullName, email, "", "")
+
+    constructor(source: Parcel) : this(
+        source.readString(),
+        source.readString(),
+        source.readString(),
+        source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(fullName)
+        writeString(email)
+        writeString(password)
+        writeString(imageUri)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<User> = object : Parcelable.Creator<User> {
+            override fun createFromParcel(source: Parcel): User = User(source)
+            override fun newArray(size: Int): Array<User?> = arrayOfNulls(size)
+        }
+    }
+}
