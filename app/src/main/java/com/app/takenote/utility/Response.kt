80228c7ed9ea882dd0fba10a85ac96775c43f7e3
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-10-14 10:55:51 +0530

    data property added

--- app/src/main/java/com/app/takenote/utility/Response.kt
+++ app/src/main/java/com/app/takenote/utility/Response.kt
@@ -2,5 +2,5 @@ package com.app.takenote.utility
 
 
 sealed class Response<T>
-class Success<T>(data: T) : Response<T>()
+class Success<T>(val data: T) : Response<T>()
 class Error<T>(val message: String) : Response<T>()
