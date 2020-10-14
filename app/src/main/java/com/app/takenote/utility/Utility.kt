4294b6eff65605b0f8840084575f88f66fa8052b
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-10-14 10:54:28 +0530

    String encryption utility method added

--- app/src/main/java/com/app/takenote/utility/Utility.kt
+++ app/src/main/java/com/app/takenote/utility/Utility.kt
@@ -2,7 +2,17 @@ package com.app.takenote.utility
 
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
+import com.tozny.crypto.android.AesCbcWithIntegrity
 
 fun View.showMessage(message: String) {
     Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
+}
+
+fun encodeString(data: String): String {
+    val salt = AesCbcWithIntegrity.saltString(AesCbcWithIntegrity.generateSalt())
+    val keys: AesCbcWithIntegrity.SecretKeys =
+        AesCbcWithIntegrity.generateKeyFromPassword(data, salt)
+    val cipherTextIvMac = AesCbcWithIntegrity.encrypt(data, keys)
+    val cipherTextString = cipherTextIvMac.toString()
+    return cipherTextString
 }
