86bc7dfab9ef3c99700dee6549686b45f3f6a781
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-10-14 10:50:03 +0530

    Profile image view added

--- app/src/main/java/com/app/takenote/ui/HomeActivity.kt
+++ app/src/main/java/com/app/takenote/ui/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.app.takenote.ui
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
 import com.app.takenote.R
+import com.app.takenote.pojo.User
+import com.app.takenote.utility.BUNDLE
+import com.app.takenote.utility.CURRENT_USER
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : BaseActivity() {
@@ -11,6 +15,15 @@ class HomeActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         searchNote.setOnFocusChangeListener { _, hasFocus ->
             searchNote.isCursorVisible = hasFocus
+        }
+        val intent = intent
+        val bundle = intent.getBundleExtra(BUNDLE)
+        val currentUser = bundle?.getParcelable<User>(CURRENT_USER)
+        currentUser?.let {
+            if (it.imageUri.isNullOrEmpty() && it.imageUri.isNullOrBlank()) {
+                profile.text = it.fullName?.get(0).toString()
+                profile.visibility = View.VISIBLE
+            }
         }
     }
 
