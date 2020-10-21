0dad55b896b8a8cd2df70c67f68a0e502fe27033
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-10-21 11:40:01 +0530

    Lifecycle extension methods added

---
+++ app/src/main/java/com/app/takenote/extensions/LifecycleExtensions.kt
@@ -0,0 +1,12 @@
+package com.app.takenote.extensions
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+fun AppCompatActivity.onBackground(body: suspend CoroutineScope.() -> Unit) {
+    lifecycleScope.launch {
+        body()
+    }
+}
