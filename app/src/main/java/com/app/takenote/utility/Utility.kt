773db2191980a711acbab308ac67020d4b18911c
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-10-23 21:53:21 +0530

    showImage and keyboard visibility function added

--- app/src/main/java/com/app/takenote/utility/Utility.kt
+++ app/src/main/java/com/app/takenote/utility/Utility.kt
@@ -1,6 +1,16 @@
 package com.app.takenote.utility
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.google.android.material.snackbar.Snackbar
 import com.tozny.crypto.android.AesCbcWithIntegrity
 
@@ -13,6 +23,44 @@ fun encodeString(data: String): String {
     val keys: AesCbcWithIntegrity.SecretKeys =
         AesCbcWithIntegrity.generateKeyFromPassword(data, salt)
     val cipherTextIvMac = AesCbcWithIntegrity.encrypt(data, keys)
-    val cipherTextString = cipherTextIvMac.toString()
-    return cipherTextString
+    return cipherTextIvMac.toString()
 }
+
+fun Context.showImage(
+    imageUrl: String,
+    targetView: ImageView,
+    onSuccess: ((message: String) -> Unit?)? = null,
+    onError: ((message: String) -> Unit?)? = null
+) {
+    Glide.with(this).load(imageUrl).apply {
+        RequestOptions().dontTransform()
+    }.listener(object : RequestListener<Drawable> {
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onSuccess?.invoke(PHOTO_UPLOADED_SUCCESSFULLY)
+            return false
+        }
+
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onError?.invoke(UNABLE_TO_UPLOAD)
+            return false
+        }
+    }).into(targetView)
+}
+
+fun Context.keyBoardVisibility(flags : Int) {
+    val inputMethodManager : InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.toggleSoftInput(flags,0)
+}
+
+
