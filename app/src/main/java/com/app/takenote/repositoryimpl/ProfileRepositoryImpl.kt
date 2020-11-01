e418c632605ea5f5aacc534f00ae747038927eea
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-11-01 08:52:34 +0530

    network check removed

--- app/src/main/java/com/app/takenote/repositoryimpl/ProfileRepositoryImpl.kt
+++ app/src/main/java/com/app/takenote/repositoryimpl/ProfileRepositoryImpl.kt
@@ -12,29 +12,25 @@ import java.io.FileInputStream
 class ProfileRepositoryImpl(private val storageReference: StorageReference) : ProfileRepository,
     BaseRepositoryImpl() {
 
-    override suspend fun uploadImage(
+    override  fun uploadImage(
         primaryId: String,
         filePath: String,
         onSuccess: (imageUrl: String) -> Unit,
         onError: (errorMessage: String) -> Unit
     ) {
-        if (isNetworkAvailable()) {
             val fileStream = stream(filePath)
             val fileReference = storageReference.child("${primaryId}/profile.jpg")
             fileReference.putStream(fileStream)
                 .addOnCompleteListener { uploadTask ->
                     if (uploadTask.isSuccessful) {
-                        fileReference.downloadUrl.addOnSuccessListener(Executors.BACKGROUND_EXECUTOR) { imageUrl ->
+                        fileReference.downloadUrl.addOnSuccessListener{ imageUrl ->
                             onSuccess(imageUrl.toString())
                         }
                     }
                 }.addOnFailureListener {
                     onError(UNABLE_TO_UPLOAD)
                 }
-        } else {
-            onError(NO_INTERNET_CONNECTION)
         }
-    }
 
     override fun clearRegisterNetworkConnection() {
         super.clearRegisterNetworkConnection()
