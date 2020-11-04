a50f327658cf086ed75dbfea82bf2f3d85ff56f6
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-11-04 13:13:35 +0530

    typewriter animation listener added

---
+++ app/src/main/java/com/app/takenote/utility/CustomTypeWriterListener.kt
@@ -0,0 +1,16 @@
+package com.app.takenote.utility
+
+import `in`.codeshuffle.typewriterview.TypeWriterListener
+
+interface CustomTypeWriterListener : TypeWriterListener {
+    override fun onCharacterTyped(text: String?, position: Int){
+    }
+
+    override fun onTypingRemoved(text: String?) {
+
+    }
+
+    override fun onTypingStart(text: String?) {
+
+    }
+}
