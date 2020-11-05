fc7837837f2dae5370010af91858794e782606ca
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-11-05 18:11:10 +0530

    source added for fire store

--- app/src/main/java/com/app/takenote/repository/DataRepository.kt
+++ app/src/main/java/com/app/takenote/repository/DataRepository.kt
@@ -1,19 +1,25 @@
 package com.app.takenote.repository
 
 import com.app.takenote.pojo.User
+import com.google.firebase.firestore.Source
 
-interface DataRepository{
+interface DataRepository {
     fun storeCurrentUserData(
-       userData : Map<String,String>,
-       primaryId: String,
+        userData: Map<String, String>,
+        primaryId: String,
         onError: (String) -> Unit
     )
 
-    fun getCurrentUserData(primaryId: String, onSuccess: (User) -> Unit, onError: (String) -> Unit)
+    fun getCurrentUserData(
+        primaryId: String,
+        onSuccess: (User) -> Unit,
+        onError: (String) -> Unit,
+        source: Source? = null
+    )
 
     fun updateData(
         primaryId: String,
-        updatedData: Map<String,String>,
+        updatedData: Map<String, String>,
         onError: ((String) -> Unit)
     )
 }
--- app/src/main/java/com/app/takenote/repositoryimpl/DataRepositoryImpl.kt
+++ app/src/main/java/com/app/takenote/repositoryimpl/DataRepositoryImpl.kt
@@ -4,28 +4,35 @@ package com.app.takenote.repositoryimpl
 import com.app.takenote.pojo.User
 import com.app.takenote.repository.DataRepository
 import com.app.takenote.utility.*
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
 
 
 class DataRepositoryImpl(private val fireStore: FirebaseFirestore) : DataRepository {
     override fun getCurrentUserData(
         primaryId: String,
         onSuccess: (User) -> Unit,
-        onError: (String) -> Unit
+        onError: (String) -> Unit,
+        source: Source?
     ) {
-        fireStore.collection(COLLECTION_NAME).document(primaryId).get()
-            .addOnSuccessListener { document ->
-                onSuccess(
-                    User(
-                        document[FULL_NAME].toString(),
-                        document[EMAIL].toString(),
-                        document[IMAGE_URL].toString(),
-                        primaryId,
-                    )
+        val task: Task<DocumentSnapshot> = if (source == null)
+            fireStore.collection(COLLECTION_NAME).document(primaryId).get()
+        else
+            fireStore.collection(COLLECTION_NAME).document(primaryId).get(source)
+        task.addOnSuccessListener { document ->
+            onSuccess(
+                User(
+                    document[FULL_NAME].toString(),
+                    document[EMAIL].toString(),
+                    document[IMAGE_URL].toString(),
+                    primaryId,
                 )
-            }.addOnFailureListener {
-                onError(SOMETHING_WENT_WRONG)
-            }
+            )
+        }.addOnFailureListener {
+            onError(SOMETHING_WENT_WRONG)
+        }
     }
 
     override fun updateData(
