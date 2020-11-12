05d324df5017c80d6275c072414702f5ecda653a
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2020-11-12 12:15:59 +0530

    parcelize annotains used

--- app/src/main/java/com/app/takenote/pojo/Note.kt
+++ app/src/main/java/com/app/takenote/pojo/Note.kt
@@ -3,7 +3,9 @@ package com.app.takenote.pojo
 import android.os.Parcel
 import android.os.Parcelable
 import com.app.takenote.utility.*
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Note(
     val id: String,
     val title: String,
@@ -12,31 +14,31 @@ data class Note(
     val generatedDate: String
 ) : Parcelable {
     constructor() : this("", "", "", "", "")
-    constructor(source: Parcel) : this(
-        source.readString()!!,
-        source.readString()!!,
-        source.readString()!!,
-        source.readString()!!,
-        source.readString()!!
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(id)
-        writeString(title)
-        writeString(body)
-        writeString(authorId)
-        writeString(generatedDate)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<Note> = object : Parcelable.Creator<Note> {
-            override fun createFromParcel(source: Parcel): Note = Note(source)
-            override fun newArray(size: Int): Array<Note?> = arrayOfNulls(size)
-        }
-    }
+//    constructor(source: Parcel) : this(
+//        source.readString()!!,
+//        source.readString()!!,
+//        source.readString()!!,
+//        source.readString()!!,
+//        source.readString()!!
+//    )
+//
+//    override fun describeContents() = 0
+//
+//    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+//        writeString(id)
+//        writeString(title)
+//        writeString(body)
+//        writeString(authorId)
+//        writeString(generatedDate)
+//    }
+//
+//    companion object {
+//        @JvmField
+//        val CREATOR: Parcelable.Creator<Note> = object : Parcelable.Creator<Note> {
+//            override fun createFromParcel(source: Parcel): Note = Note(source)
+//            override fun newArray(size: Int): Array<Note?> = arrayOfNulls(size)
+//        }
+//    }
 
     fun toMap(): Map<String, String> {
         return mutableMapOf(
--- app/src/main/java/com/app/takenote/pojo/User.kt
+++ app/src/main/java/com/app/takenote/pojo/User.kt
@@ -1,8 +1,10 @@
 package com.app.takenote.pojo
 
-import android.os.Parcel
-import android.os.Parcelable
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class User(
     val fullName: String?,
     val email: String?,
@@ -20,29 +22,29 @@ data class User(
 
     constructor(email: String?, uid: String?, imageUri: String?) : this("", email, "", imageUri, uid)
 
-    constructor(source: Parcel) : this(
-        source.readString(),
-        source.readString(),
-        source.readString(),
-        source.readString(),
-        source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(fullName)
-        writeString(email)
-        writeString(password)
-        writeString(imageUri)
-        writeString(uid)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<User> = object : Parcelable.Creator<User> {
-            override fun createFromParcel(source: Parcel): User = User(source)
-            override fun newArray(size: Int): Array<User?> = arrayOfNulls(size)
-        }
-    }
+//    constructor(source: Parcel) : this(
+//        source.readString(),
+//        source.readString(),
+//        source.readString(),
+//        source.readString(),
+//        source.readString()
+//    )
+//
+//    override fun describeContents() = 0
+//
+//    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+//        writeString(fullName)
+//        writeString(email)
+//        writeString(password)
+//        writeString(imageUri)
+//        writeString(uid)
+//    }
+//
+//    companion object {
+//        @JvmField
+//        val CREATOR: Parcelable.Creator<User> = object : Parcelable.Creator<User> {
+//            override fun createFromParcel(source: Parcel): User = User(source)
+//            override fun newArray(size: Int): Array<User?> = arrayOfNulls(size)
+//        }
+//    }
 }
