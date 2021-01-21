36a6e3a1e8d6b9829546e3d939d8658a718f3aa4
AvishekThapa1997 <61781142+AvishekThapa1997@users.noreply.github.com>
2021-01-21 14:22:32 +0530

    updated

--- app/src/main/java/com/app/takenote/pojo/Note.kt
+++ app/src/main/java/com/app/takenote/pojo/Note.kt
@@ -1,6 +1,5 @@
 package com.app.takenote.pojo
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.app.takenote.utility.*
 import kotlinx.android.parcel.Parcelize
@@ -11,34 +10,10 @@ data class Note(
     val title: String,
     val body: String,
     val authorId: String,
-    val generatedDate: String
+    val generatedDate: String,
+    val reminderTime: String,
 ) : Parcelable {
-    constructor() : this("", "", "", "", "")
-//    constructor(source: Parcel) : this(
-//        source.readString()!!,
-//        source.readString()!!,
-//        source.readString()!!,
-//        source.readString()!!,
-//        source.readString()!!
-//    )
-//
-//    override fun describeContents() = 0
-//
-//    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-//        writeString(id)
-//        writeString(title)
-//        writeString(body)
-//        writeString(authorId)
-//        writeString(generatedDate)
-//    }
-//
-//    companion object {
-//        @JvmField
-//        val CREATOR: Parcelable.Creator<Note> = object : Parcelable.Creator<Note> {
-//            override fun createFromParcel(source: Parcel): Note = Note(source)
-//            override fun newArray(size: Int): Array<Note?> = arrayOfNulls(size)
-//        }
-//    }
+    constructor() : this("", "", "", "", "", "")
 
     fun toMap(): Map<String, String> {
         return mutableMapOf(
@@ -46,7 +21,8 @@ data class Note(
             NOTE_TITLE to title,
             NOTE_BODY to body,
             AUTHOR_ID to authorId,
-            GENERATED_DATE to generatedDate
+            GENERATED_DATE to generatedDate,
+            REMINDER_TIME to reminderTime
         )
     }
 }
